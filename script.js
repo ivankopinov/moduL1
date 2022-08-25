072f16471e26f59b9c3a92b360a4fc81c4b33db8
Иван Копинов <ikopinov@gmail.com>
2022-08-25 11:28:09 +0500

    les2

--- script.js
+++ script.js
@@ -7,11 +7,3 @@
   const productPrice = "8499";
   console.log("Наименование товара:", productName);
 }
-
-{
-  const productName = "Микроволновая печь Midea";
-  const productAmount = "100";
-  const productCategory = "Техника для кухни";
-  const productPrice = "4590";
-  console.log("Наименование товара:", productName);
-}
