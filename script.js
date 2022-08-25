a4a66762f7b4ad10a5f86a7f0d51028b8ae821e5
Иван Копинов <ikopinov@gmail.com>
2022-08-25 11:23:05 +0500

    lesson2

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+{
+  const productName = "Пальто";
+  const productAmount = "20";
+  const productCategory = "Мужская верхняя одежда";
+  const productPrice = "8499";
+  console.log("Наименование товара:", productName);
+}
+
+{
+  const productName = "Микроволновая печь Midea";
+  const productAmount = "100";
+  const productCategory = "Техника для кухни";
+  const productPrice = "4590";
+  console.log("Наименование товара:", productName);
+}
