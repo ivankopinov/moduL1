e4015148782faabf41e4229d36135bdbc84abfc2
Иван Копинов <ikopinov@gmail.com>
2022-08-26 17:55:33 +0500

    2goods

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 "use strict";
 
 {
-  const productName = "Пальто";
-  const productAmount = "20";
+  const productName = "Наименование товара: Пальто";
+  const productAmount = 20;
   const productCategory = "Мужская верхняя одежда";
-  const productPrice = "8499";
-  console.log("Наименование товара:", productName);
+  const productPrice = 8499;
+  console.log(productName);
+}
+
+{
+  const productName = "Наименование товара: Микроволновая печь Samsung";
+  const productAmount = 50;
+  const productCategory = "Техника для кухни";
+  const productPrice = 4590;
+  console.log(productName);
 }
