d16fc20579055338d2d6b78ee675aa1c25fb8025
Иван Копинов <ikopinov@gmail.com>
2022-08-29 15:03:32 +0500

    lesson4

---
+++ 4lesson/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+{
+  let userProduct = prompt(`Введите наименование товара?`);
+  let userCategory = prompt(`Введите категорию товара?`);
+  let userPrice = Number(prompt(`Введите стоимость товара?`));
+  let userAmount = Number(prompt(`Введите количество товара?`));
+  if ((userAmount, userPrice)) {
+    console.log(
+      "На складе " +
+        userAmount +
+        " единицы товара " +
+        userProduct +
+        " на сумму " +
+        userAmount * userPrice +
+        " рублей"
+    );
+  } else {
+    console.log("Вы ввели некорректные данные");
+  }
+}
+
+/* {
+  const productName = "Наименование товара: Пальто";
+  const productAmount = 20;
+  const productCategory = "Мужская верхняя одежда";
+  const productPrice = 8499;
+  console.log(productName);
+}
+
+{
+  const productName = "Наименование товара: Микроволновая печь Samsung";
+  const productAmount = 50;
+  const productCategory = "Техника для кухни";
+  const productPrice = 4590;
+  console.log(productName);
+}
+ */
---
+++ 4lesson/tasks.js
@@ -0,0 +1,8 @@
+const rain = Math.round(Math.random());
+console.log(rain);
+
+if (rain === 0) {
+  console.log("Дождя нет!");
+} else {
+  console.log("Пошёл дождь. Возьмите зонт!");
+}
