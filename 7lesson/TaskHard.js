1cd67210684eb8b94e2f0fb44fca4057e49454c2
Иван Копинов <ikopinov@gmail.com>
2022-09-04 19:12:57 +0500

    7less

--- 7lesson/TaskHard.js
+++ 7lesson/TaskHard.js
@@ -1,17 +1,17 @@
 "use strict";
 
 const allСashbox = [
-  [12, 4500], 
-  [7, 3210], 
-  [4, 650], 
-  [3, 1250], 
-  [9, 7830], 
-  [1, 990], 
-  [6, 13900], 
-  [1, 370]
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
 ];
 
-const [[x, y, ...arr]] = allСashbox;
-console.log();
-
-
+allСashbox.forEach((i) => {
+  const getAveragePriceGoods = i[1] / i[0];
+  console.log(getAveragePriceGoods);
+});
